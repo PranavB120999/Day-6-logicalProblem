fb65168e0d3021abfc30d9a231a385220dc0b846
Pranav <pranavbhople99@gmail.com>
2022-03-18 14:35:53 +0530

    pranav Bhople main file for reverse Number

--- Program.cs
+++ Program.cs
@@ -1,37 +1,28 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Threading;
 
 namespace Day6LogicalProblem
 {
     internal class Program
     {
-        public static void CouponNumber()
+        public static void ReverseNumber()
         {
-            Greeting message for logical program
-            Console.WriteLine("Please Enter how many Random distinct number you want: ");
+            Console.WriteLine("Please Enter a Number To check Reverese Number: ");
             int num = int.Parse(Console.ReadLine());
-            int[] ran = new int[num];
-            Console.WriteLine("Enter the Starting range of Random Number you want");
-            int str = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the ending range of Random numnber you want ");
-            int end = int.Parse(Console.ReadLine());
-            for (int i = 0; i < num; i++)
+            int temp, rem, rev = 0;
+            temp = num;
+            while (num > 0)
             {
-                Random random = new Random();
-                int n = random.Next(str, end);
-                ran[i] = n;
+                rem = num % 10;
+                rev = ((rev * 10) + rem);
+                num = num / 10;
             }
-            Console.WriteLine("All Random Distinct Numbers are: ");
-            foreach (int j in ran)
-            {
-                Console.WriteLine(j);
-            }
+
+            Console.WriteLine("Reverse of the number " + temp + " is: " + rev);
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Welcome to Coupon Number Program");
-            Program.CouponNumber();
+            Console.WriteLine("Hello, Welcome to Reverse number Program");
+            Program.ReverseNumber();
         }
     }
 }
